a5b3e14c043842adbc2934155b4a007a1690a889
pg22nicholas <pg22nicholas@vfs.com>
2022-05-07 13:40:56 -0700

    Added dolly track functionality to player parent movement

--- Assets/Scripts/Player/PlayerParentMovement.cs
+++ Assets/Scripts/Player/PlayerParentMovement.cs
@@ -1,26 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
-public class PlayerParentMovement : MonoBehaviour
+public class PlayerParentMovement : CinemachineDollyCart
 {
-    [Tooltip("Movement speed forward")]
-    [SerializeField] private float m_MovementSpeed = 25f;
+    [Tooltip("The base speed to follow the path, determines Speed field")]
+    [SerializeField] private float m_BaseSpeed = 10f;
 
     [Header("Dash")]
     [Tooltip("Speed percentage increase forward")]
     [SerializeField] private float m_SpeedIncreasePercent = 0.5f;
 
-    private float m_CurrMovementSpeed;  // Current movement speed forward
-
     private void Start()
     {
-        m_CurrMovementSpeed = m_MovementSpeed;
+        m_Speed = m_BaseSpeed;
     }
 
-    void Update()
+    private void Update()
     {
-        transform.transform.Translate(Vector3.forward * m_CurrMovementSpeed * Time.deltaTime);
+        transform.transform.Translate(Vector3.forward * m_Speed * Time.deltaTime);
     }
 
     public void PerformDash(float duration)
@@ -32,13 +31,13 @@ public class PlayerParentMovement : MonoBehaviour
     {
         float currDuration = 0f;
 
-        m_CurrMovementSpeed += m_CurrMovementSpeed * m_SpeedIncreasePercent;
+        m_Speed += m_Speed * m_SpeedIncreasePercent;
         while (currDuration < duration)
         {
             currDuration += Time.deltaTime;
             yield return null;
         }
 
-        m_CurrMovementSpeed = m_MovementSpeed;
+        m_Speed = m_BaseSpeed;
     }
 }
