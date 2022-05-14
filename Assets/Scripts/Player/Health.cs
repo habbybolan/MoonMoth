0d9530e23b7baed173be62aa8463c2050c6bc24b
pg22nicholas <pg22nicholas@vfs.com>
2022-05-14 12:16:46 -0700

    Added basic functionality to Health class

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private int m_currentHealth = 1;
+    [SerializeField] private ParticleSystem m_deathParticles;
+    [SerializeField] private float m_deathParticlesDuration = 1f;
+    [SerializeField] private UnityEvent m_OnDeathEvent;
 
+    private int m_maxHealth;
+
+    public float HealthPercentage => (float)m_currentHealth / m_maxHealth;
+
+    private void Start()
+    {
+        m_maxHealth = m_currentHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Damage(int amount)
     {
+        m_currentHealth -= amount;
+        if (m_currentHealth <= 0)
+        {
+            Death();
+        }
+    }
 
+    private void Death()
+    {
+        if (m_deathParticles != null)
+        {
+            Transform currentTransform = transform;
+            Destroy(Instantiate(m_deathParticles, currentTransform.position, currentTransform.rotation), m_deathParticlesDuration);
+        }
+        m_OnDeathEvent?.Invoke();
     }
 }
