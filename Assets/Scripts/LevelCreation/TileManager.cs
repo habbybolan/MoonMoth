6a77ee7c0a227979999711f9c1a87dd457d27d7e
pg22nicholas <pg22nicholas@vfs.com>
2022-05-17 17:55:01 -0700

    Fixed rotation of firefly so it's facing player

--- Assets/Scripts/LevelCreation/TileManager.cs
+++ Assets/Scripts/LevelCreation/TileManager.cs
@@ -10,7 +10,7 @@ public class TileManager : MonoBehaviour
     [SerializeField] private float m_DistanceToPlaceTile = 200f;
     [SerializeField] private float m_DistanceToDeleteTile = 50f;
 
-    private Tile[] m_PoolTiles = new Tile[30];
+    private Tile[] m_PoolTiles = new Tile[40];
     private LinkedList<Tile> m_VisibleTiles = new LinkedList<Tile>();
 
     public static TileManager PropertyInstance
