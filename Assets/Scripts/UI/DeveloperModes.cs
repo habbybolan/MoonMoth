eaebc716506c6177795002805f3d31c8225a77a3
pg22nicholas <pg22nicholas@vfs.com>
2022-05-29 22:47:59 -0700

    Created Developer Mode script for calling debug methods, attached stubbed methods to UI elements

---
+++ Assets/Scripts/UI/DeveloperModes.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeveloperModes : MonoBehaviour
+{
+
+    public void ToggleInvinicibility(bool isInvincible)
+    {
+        // TODO:
+    }
+
+    public void ToggleUnlimitedMoonBar(bool isUnlimitedMoonBar)
+    {
+        // TODO:
+    }
+
+    public void ToggleNoClip(bool isNoClip)
+    { 
+        // TODO:
+    }
+
+    public void SpawnFirefly()
+    {
+        // TODO:
+    }
+
+    public void ResetWorld()
+    {
+        // TODO:
+    }
+
+}
