f39e3b5465519730a72a515d6f814225c2e2fdfc
pg22nicholas <pg22nicholas@vfs.com>
2022-05-31 14:31:30 -0700

    Added projectile sounds and implemented
    changed the rate of fire to be slower on everything

--- Assets/Scripts/Weapons/WeaponBase.cs
+++ Assets/Scripts/Weapons/WeaponBase.cs
@@ -6,11 +6,19 @@ public class WeaponBase : MonoBehaviour
 {
     [SerializeField] protected float m_Cooldown = 0.25f;
     [SerializeField] protected Projectile m_BulletPrefab;
+    [SerializeField] private Vector2 m_PitchRange = new Vector2(0.9f, 1.1f); 
+
+    private AudioSource m_ProjectileAudio;
 
     // Owner that contains the tag (the 'team' they are on)
     [SerializeField] protected Health m_WeaponOwner;
 
     protected bool isCooldown = false;
+
+    private void Awake()
+    {
+        m_ProjectileAudio = GetComponent<AudioSource>();
+    }
 
     public void ShootPosition(Vector3 fireAtLocation)
     {
@@ -24,6 +32,10 @@ public class WeaponBase : MonoBehaviour
 
         Projectile projectileShot = Instantiate(m_BulletPrefab, transform.position, Quaternion.LookRotation(shootDirection));
         projectileShot.Owner = m_WeaponOwner.gameObject;
+
+        m_ProjectileAudio.pitch = Random.Range(m_PitchRange.x, m_PitchRange.y);
+        m_ProjectileAudio.Play();
+
         StartCoroutine(WeaponCooldown());
     }
 
