8a177931ec66985cb0a2da5dc771e5bde4e6ef28
pg22nicholas <pg22nicholas@vfs.com>
2022-06-03 21:54:17 -0700

    Added a game manager to handle large game events and transitions

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Manages the main game state
+//  Includes transitions between levels, player death
+public class GameManager : MonoBehaviour
+{ 
+    static GameManager s_PropertyInstance;
+    public static GameManager PropertyInstance
+    {
+        get { return s_PropertyInstance; }
+    }
+
+    private void Awake()
+    {
+        // Singleton
+        if (s_PropertyInstance != null && s_PropertyInstance != this)
+            Destroy(this);
+        else
+            s_PropertyInstance = this;
+    }
+
+    // On player dying, lose
+    public void OnGameOver()
+    {
+        GameState.m_GameState = GameStateEnum.LOST;
+        SceneManager.LoadScene("WinLose");
+    }
+
+    // On player winning the game
+    public void OnGameWon()
+    {
+        GameState.m_GameState = GameStateEnum.WON;
+        SceneManager.LoadScene("WinLose");
+    }
+
+    // Enough lost moths collected to either goto next level or game finished
+    public void OnAllLostMothsCollected()
+    {
+        // if last tile set finished, then game won
+        if (TileManager.PropertyInstance.TileSetFinished())
+        {
+            OnGameWon();
+            return;
+        }
+        GameState.m_GameState = GameStateEnum.TRANSITIONING;
+        FireflyManager.PropertyInstance.TileSetFinished();
+    }
+}
