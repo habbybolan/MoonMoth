cf0e11bef2f0de5b4d1b5b27374e99d82c80d8dc
pg22nicholas <pg22nicholas@vfs.com>
2022-06-07 13:21:08 -0700

    Emission scaled with health percentage
    Health loss per second changed to apply damage instead of directly removing health

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
 
 /*
  * Deals with any object that can interact and damage the player.
@@ -13,19 +14,22 @@ public class PlayerHealth : Health
     [Range(0f, 100f)]
     [SerializeField] private float m_TerrainDamageAmount = 5f;
     [SerializeField] private TextMeshProUGUI m_HealthText;
-    [SerializeField] private GameObject m_MothBody; 
+    [SerializeField] private Renderer[] m_EmissionRenderer;
+    [SerializeField] private float m_EmissionIntensity = 3.5f;
     
     private HEALTH_STATE healthState;       // If the player can be damaged or not by non-terrain damage types
-
-    private void Awake()
-    {
-    }
+    private float m_MaxEmissionRGB;
+    private float m_RGBDifference;
+    private Color m_StartingEmissionColor;
 
     protected override void Start()
     { 
         healthState = HEALTH_STATE.VULNERABLE;
         base.Start();
         SetHealthText();
+
+        m_StartingEmissionColor = m_EmissionRenderer[0].material.GetColor("_EmissionColor");
+        m_MaxEmissionRGB = Mathf.Max(m_StartingEmissionColor.r, m_StartingEmissionColor.g, m_StartingEmissionColor.b); 
     }
 
     public override void Damage(DamageInfo damageInfo)
@@ -35,6 +39,7 @@ public class PlayerHealth : Health
         {
             base.Damage(damageInfo);
             SetHealthText();
+            UpdateEmission();
             return;
         }
          
@@ -48,6 +53,22 @@ public class PlayerHealth : Health
             StartCoroutine(InvulnerabilityFrames());
         }
         SetHealthText();
+    }
+
+    // Scale the emission amount of moth with their health percentage
+    private void UpdateEmission()
+    {
+        foreach (Renderer renderer in m_EmissionRenderer)
+        {
+            Color currColor = renderer.material.GetColor("_EmissionColor");
+            float LerpValueOfMaxRGB = Mathf.Lerp(1, m_MaxEmissionRGB, HealthPercentage);
+            float differenceFromMax = m_MaxEmissionRGB - LerpValueOfMaxRGB;
+            Color newColor = new Color(m_StartingEmissionColor.r - differenceFromMax,
+                                        m_StartingEmissionColor.g - differenceFromMax,
+                                        m_StartingEmissionColor.b - differenceFromMax,
+                                        currColor.a);
+            renderer.material.SetColor("_EmissionColor", newColor);
+        }
     }
 
     private void SetHealthText()
