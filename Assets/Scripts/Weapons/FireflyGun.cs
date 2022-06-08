c5c3ac355b6e51d52728da12ef49fb1dffd32928
pg22nicholas <pg22nicholas@vfs.com>
2022-06-08 13:59:47 -0700

    Fully implemeneted homing shot on firefly

--- Assets/Scripts/Weapons/FireflyGun.cs
+++ Assets/Scripts/Weapons/FireflyGun.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireflyGun : EnemyWeapon<Projectile>
+public class FireflyGun : EnemyWeapon<HomingProjectile>
 {
-       
+    protected override void Shoot()
+    {
+        base.Shoot();
+        if (m_LastShotProjectile != null)
+        {
+            m_LastShotProjectile.SetTarget(PlayerManager.PropertyInstance.PlayerController.Health);
+        }
+    }
 }
