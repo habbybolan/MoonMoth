bc7db7114c9b97adae89240c79102ea043f74979
pg22nicholas <pg22nicholas@vfs.com>
2022-06-08 00:59:02 -0700

    Fixed a bunch of prefabs with weapons and projectils, improved shooting and collision
    updated a bunch of weapon and enemy field values

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -6,11 +6,17 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] protected float m_Speed = 100f;
     [SerializeField] protected float m_DamageAmount = 5f;
-    [SerializeField] protected Rigidbody m_rigidBody;
     [SerializeField] protected DamageInfo.HIT_EFFECT m_HitEffect = DamageInfo.HIT_EFFECT.NORMAL;
     [SerializeField] protected float m_Duration = 10f;
 
+    protected Rigidbody m_rigidBody;
+
     protected GameObject m_Owner;
+
+    private void Awake()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
 
     // Start is called before the first frame update
     protected virtual void Start()
@@ -37,9 +43,13 @@ public class Projectile : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+       
         Health health = other.gameObject.GetComponent<Health>();
         if (health != null)
         {
+            // prevent instigator from hitting self
+            if (m_Owner == health.gameObject) return;
+
             DamageInfo damageInfo = new DamageInfo(m_DamageAmount, m_Owner, health.gameObject, DamageInfo.DAMAGE_TYPE.PROJECTILE, m_HitEffect);
             health.Damage(damageInfo);
             CollisionPoint(other);
