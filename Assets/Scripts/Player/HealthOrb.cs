20bb884a7a76481d6c6ecb9d778ffd5b1fa1dc02
pg22nicholas <pg22nicholas@vfs.com>
2022-06-09 17:00:38 -0700

    Health orb flies up and homes to player  - HealthOrb updated to be a homing projectile

--- Assets/Scripts/Player/HealthOrb.cs
+++ Assets/Scripts/Player/HealthOrb.cs
@@ -2,11 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthOrb : MonoBehaviour
+public class HealthOrb : HomingProjectile
 {
+    [Header("Health Orb")]
     [SerializeField] private float m_healthRecovery = 10f;
+    [SerializeField] private float m_FlyUpDuration = 1f;
+    [SerializeField] private float m_FlyUpSpeed = 3f;
 
-    private void OnTriggerEnter(Collider other)
+    protected override void Start()
+    {
+        base.Start();
+        StartCoroutine(GoUp());
+        
+        // start with no velocity
+        m_rigidBody.velocity = Vector3.up * m_FlyUpSpeed;
+        
+    }
+
+    protected override void OnTriggerEnter(Collider other)
     {
         PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
         if (playerHealth != null)
@@ -14,5 +27,21 @@ public class HealthOrb : MonoBehaviour
             playerHealth.HealAmount(m_healthRecovery);
             Destroy(gameObject);
         }
+    }
+
+    // Sends the object up for a time
+    IEnumerator GoUp()
+    {
+        float currDuration = 0;
+        while (currDuration < m_FlyUpDuration)
+        {
+            // TODO:
+            currDuration += Time.deltaTime;
+            yield return null;
+        }
+        m_Target = PlayerManager.PropertyInstance.PlayerController.Health;
+        // look at the player
+        transform.rotation = Quaternion.LookRotation(PlayerManager.PropertyInstance.PlayerController.transform.position - transform.position);
+        Shoot();
     }
 }
