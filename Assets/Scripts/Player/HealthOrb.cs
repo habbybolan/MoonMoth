3c04bc676e70cc6e921a8ae7c1f63930d26c1ef1
pg22nicholas <pg22nicholas@vfs.com>
2022-06-12 16:23:50 -0700

    Updated HealthOrb and made particles not die out instantly

--- Assets/Scripts/Player/HealthOrb.cs
+++ Assets/Scripts/Player/HealthOrb.cs
@@ -9,9 +9,12 @@ public class HealthOrb : HomingProjectile
     [SerializeField] private float m_FlyUpDuration = 1f;
     [SerializeField] private float m_FlyUpSpeed = 3f;
 
+    private ParticleSystem m_TrailParticles;
+
     protected override void Start()
     {
         base.Start();
+        m_TrailParticles = GetComponentInChildren<ParticleSystem>();
         StartCoroutine(GoUp());
         
         // start with no velocity
@@ -24,8 +27,20 @@ public class HealthOrb : HomingProjectile
         if (playerHealth != null)
         {
             playerHealth.HealAmount(m_healthRecovery);
+            DetachParticles();
             Destroy(gameObject);
         }
+    }
+
+    private void DetachParticles()
+    {
+        m_TrailParticles.transform.parent = null;
+
+        m_TrailParticles.Stop();
+
+        // This finds the particleAnimator associated with the emitter and then
+        // sets it to automatically delete itself when it runs out of particles
+        //m_TrailParticles.GetComponent<ParticleAnimator>().autoDestruct = true;
     }
 
     // Sends the object up for a time
