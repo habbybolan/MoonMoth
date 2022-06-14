302894c20d3a8cbe19cdf0e4fb7b5a1b6d5e64cc
pg22nicholas <pg22nicholas@vfs.com>
2022-06-14 14:03:17 -0700

    Added fade in/out methods in UIManager

--- Assets/Scripts/Level/UIManager.cs
+++ Assets/Scripts/Level/UIManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 // Singleton fog manager for changes the fog color based on the current level
 public class UIManager : MonoBehaviour
@@ -8,6 +10,14 @@ public class UIManager : MonoBehaviour
     [Header("Atmospheric fog")]
     [SerializeField] private Color[] m_FogLevelColors;
     [SerializeField] private Color m_DefaultFogColor = new Color(185, 181, 171, 255);
+
+    [Header("Screen fade")]
+    [SerializeField] private Image m_FadeScreen;
+
+    private bool m_IsFadeIn;
+    private Coroutine m_FadeInCoroutine;
+    private bool m_IsFadeOut;
+    private Coroutine m_FadeOutCoroutine;
 
     static UIManager s_PropertyInstance;
     public static UIManager PropertyInstance
@@ -44,5 +54,63 @@ public class UIManager : MonoBehaviour
     private void UpdateFog(Color color)
     {
         RenderSettings.fogColor = color;
+    }
+
+    // Fade screen into black
+    public void FadeIn(float fadeInDuration)
+    {
+        // prevent double fading in
+        if (m_IsFadeIn) return;
+
+        // if currently fading out, cancel and start fading in
+        if (m_IsFadeOut)
+        {
+            StopCoroutine(m_FadeOutCoroutine);
+            m_IsFadeOut = false;
+        }
+        m_FadeInCoroutine = StartCoroutine(FadeInCoroutine(fadeInDuration));
+    }
+
+    private IEnumerator FadeInCoroutine(float fadeInDuration)
+    {
+        m_IsFadeIn = true;
+        float elapsedTime = 0.0f; 
+        Color startColor = m_FadeScreen.color;
+        while (elapsedTime < fadeInDuration)
+        {
+            m_FadeScreen.color = Color.Lerp(startColor,Color.black, (elapsedTime / fadeInDuration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        m_IsFadeIn = false;
+    }
+
+    public void FadeOut(float fadeOutDuration) 
+    {
+        // prevent double fading out
+        if (m_IsFadeOut) return;
+
+        // if currently fading in, cancel and start fading out
+        if (m_IsFadeIn)
+        {
+            StopCoroutine(m_FadeInCoroutine);
+            m_IsFadeIn = false;
+        }
+        m_FadeOutCoroutine = StartCoroutine(FadeOutCoroutine(fadeOutDuration));
+    }
+     
+    // fade screen out of black 
+    private IEnumerator FadeOutCoroutine(float fadeOutDuration)
+    {
+        m_IsFadeOut = true;
+        float elapsedTime = 0.0f;
+        Color startColor = m_FadeScreen.color;
+        while (elapsedTime < fadeOutDuration)
+        {
+            m_FadeScreen.color = Color.Lerp(startColor, Color.clear, (elapsedTime / fadeOutDuration));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        m_IsFadeOut = false;
     }
 }
