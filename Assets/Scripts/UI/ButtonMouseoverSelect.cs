5ddbbab9602c1aedaa2022bbe5ffa2378fea4b2d
pg22nicholas <pg22nicholas@vfs.com>
2022-06-13 23:32:47 -0700

    updated mouse event script to deal with all highlight events for mouse and controller
    Button has proper interactions and visually updates correctly

--- Assets/Scripts/UI/ButtonMouseoverSelect.cs
+++ Assets/Scripts/UI/ButtonMouseoverSelect.cs
@@ -3,18 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
-public class ButtonMouseoverSelect : MonoBehaviour, IPointerEnterHandler
+public class ButtonMouseoverSelect : Button, IPointerEnterHandler, IPointerExitHandler
 {
-    private Button _button;
+    private Button m_Button;
+    private TextMeshProUGUI m_ButtonLabel;
+    [SerializeField] private Image m_SelectIcon;
 
-    private void Start()
+    BaseEventData m_BaseEvent;
+
+    public Color m_DefaultColor = new Color(.5f, .5f, .5f, .5f);
+    public Color m_SelectedColor = Color.white;
+
+    protected override void Awake()
     {
-        _button = GetComponent<Button>();
+        m_Button = GetComponent<Button>();
+        m_ButtonLabel = GetComponentInChildren<TextMeshProUGUI>();
+
+        if (GetComponent<ButtonAutoSelect>())
+        {
+            ButtonSelected();
+        }
+        else ButtonUnselected();
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    private void Update()
+    {
+        if (gameObject == EventSystem.current.currentSelectedGameObject) ButtonSelected();
+        else ButtonUnselected();
+    }
+
+    public override void OnPointerEnter(PointerEventData eventData)
     {
         EventSystem.current.SetSelectedGameObject(gameObject);
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        ButtonUnselected();
+    }
+
+    private void ButtonSelected()
+    {
+        m_ButtonLabel.color = m_SelectedColor;
+        m_SelectIcon.gameObject.SetActive(true);
+    }
+
+    private void ButtonUnselected()
+    {
+        m_ButtonLabel.color = m_DefaultColor;
+        m_SelectIcon.gameObject.SetActive(false);
     }
 }
