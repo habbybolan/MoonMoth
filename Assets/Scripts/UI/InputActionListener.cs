475619bcc44aac503e8ef89a04e1f27b1c5cc112
pg22nicholas <pg22nicholas@vfs.com>
2022-06-21 13:57:44 -0700

    added controls panel in main menu

--- Assets/Scripts/UI/InputActionListener.cs
+++ Assets/Scripts/UI/InputActionListener.cs
@@ -15,12 +15,14 @@ public class InputActionListener : MonoBehaviour
 
     private void OnEnable()
     {
-        _action.action.performed += Performed;
+        if (_action != null)
+            _action.action.performed += Performed;
     }
 
     private void OnDisable()
     {
-        _action.action.performed -= Performed;
+        if (_action != null)
+            _action.action.performed -= Performed;
     }
 
     private void Performed(InputAction.CallbackContext ctx)
