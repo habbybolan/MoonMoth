6f3a585d022d570ebf8e272b9f985945a1163501
pg22nicholas <pg22nicholas@vfs.com>
2022-12-11 20:25:45 -0800

    Added extra homing to homing projectile when in certain range of target

--- Assets/Scripts/Weapons/HomingProjectile.cs
+++ Assets/Scripts/Weapons/HomingProjectile.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class HomingProjectile : Projectile
 {
     [SerializeField] private float m_MaxHomingAngle = 1f;
+    [SerializeField] private float m_DistToIncrHomingAngle = 20;
+    [Min(1)]
+    [SerializeField] private float m_MaxHomingAngleInRange = 1f;
 
     protected Health m_Target;
 
@@ -31,6 +34,13 @@ public class HomingProjectile : Projectile
         Vector3 targetPos = m_Target.transform.position;
         Vector3 directionToTarget = targetPos - transform.position;
         Quaternion rotationToTarget = Quaternion.FromToRotation(Vector3.forward, directionToTarget);
+
+        float homingAngle = m_MaxHomingAngle;
+        float dist = Vector3.Distance(targetPos, transform.position);
+        // check to increase homing amount when closer to target
+        if (dist <= m_DistToIncrHomingAngle)
+            homingAngle = 1 + (dist / m_DistToIncrHomingAngle) * (m_MaxHomingAngleInRange - 1);
+
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationToTarget, m_MaxHomingAngle);
         m_rigidBody.velocity = transform.forward * m_Speed;
     }
