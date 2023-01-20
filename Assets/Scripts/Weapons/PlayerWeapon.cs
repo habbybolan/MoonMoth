1a3de303c98d4ba9d4b803f498cc9f58310a047b
pg22nicholas <pg22nicholas@vfs.com>
2023-01-20 01:15:30 -0800

    fixed player projectile targeting terrain/obstacle behind the player moth

--- Assets/Scripts/Weapons/PlayerWeapon.cs
+++ Assets/Scripts/Weapons/PlayerWeapon.cs
@@ -7,18 +7,14 @@ public class PlayerWeapon : WeaponBase<HomingProjectile>
     [SerializeField] private float m_ShootCastRadius = 10f;
     private PlayerController m_Controller;      // Player Controller
     private bool m_IsShooting = false;
-    private int m_AvoidLayerMask;
+    private int m_TargetableLayerMask;
 
     private void Start()
     {
         m_Controller = PlayerManager.PropertyInstance.PlayerController;
-        m_AvoidLayerMask = 1 << LayerMask.NameToLayer("PlayerControl");
-        m_AvoidLayerMask = m_AvoidLayerMask | 1 << LayerMask.NameToLayer("Player");
-        m_AvoidLayerMask = m_AvoidLayerMask | 1 << LayerMask.NameToLayer("Projectile");
-        m_AvoidLayerMask = m_AvoidLayerMask | 1 << LayerMask.NameToLayer("Obstacle");
-        m_AvoidLayerMask = m_AvoidLayerMask | 1 << LayerMask.NameToLayer("Terrain");
-        m_AvoidLayerMask = m_AvoidLayerMask | 1 << LayerMask.NameToLayer("Default");
-        m_AvoidLayerMask = ~m_AvoidLayerMask;
+        m_TargetableLayerMask = 1 << LayerMask.NameToLayer("Terrain");
+        m_TargetableLayerMask = m_TargetableLayerMask | 1 << LayerMask.NameToLayer("Obstacle");
+        m_TargetableLayerMask = m_TargetableLayerMask | 1 << LayerMask.NameToLayer("Enemy");
     }
 
     public void TryShoot()
@@ -26,20 +22,33 @@ public class PlayerWeapon : WeaponBase<HomingProjectile>
         if (!m_IsShooting) return;
 
         Ray crosshairRay = m_Controller.PlayerMovement.CrosshairScreenRay;
-        RaycastHit hit;
-        if (Physics.SphereCast(crosshairRay, m_ShootCastRadius, out hit, Mathf.Infinity, m_AvoidLayerMask))
+
+        // Check if an enemy is near the crosshair
+        int m_EnemyLayerMask = 1 << LayerMask.NameToLayer("Enemy");
+        RaycastHit[] hits = Physics.SphereCastAll(crosshairRay, m_ShootCastRadius, Mathf.Infinity, m_EnemyLayerMask);
+        if (hits.Length > 0)
         {
-            ShootPosition(hit.point);
+            // TODO: Get closest enemy and check if enemy is not obstructed from this weapon?
+            ShootPosition(hits[0].point);
             // if spherecast hits enemy, apply target to Homing projectile
-            Health health = hit.collider.gameObject.GetComponent<Health>();
+            Health health = hits[0].collider.gameObject.GetComponent<Health>();
             if (m_LastShotProjectile != null && health != null)
             {
                 m_LastShotProjectile.SetTarget(health);
             }
-        }
+        } 
+        // Otherwise, shoot towards terrain/obstacle, or generally forward
         else
         {
-            ShootDirection(crosshairRay.direction);
+            RaycastHit hit;
+            if (Physics.Raycast(crosshairRay, out hit, Mathf.Infinity, m_TargetableLayerMask))
+            {
+                ShootPosition(hit.point);
+            }
+            else
+            {
+                ShootDirection(crosshairRay.direction);
+            }
         }
     }
 
