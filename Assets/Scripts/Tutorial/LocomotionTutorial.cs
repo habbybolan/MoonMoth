7fc356232b31b7dbff7634754386670413c6d60d
pg22nicholas <pg22nicholas@vfs.com>
2023-01-26 22:40:40 -0800

    Created stubbed Locomotion tutorial

---
+++ Assets/Scripts/Tutorial/LocomotionTutorial.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionTutorial : Tutorial
+{
+    public override void SetupTutorial()
+    {
+        base.SetupTutorial();
+
+
+    }
+
+    public override void EndTutorialLogic()
+    {
+        base.EndTutorialLogic();
+    }
+}
