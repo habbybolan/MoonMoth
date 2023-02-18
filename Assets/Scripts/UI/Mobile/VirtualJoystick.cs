26960ee47c3515fceb3a37a9b02fe9c618d3c6ce
pg22nicholas <pg22nicholas@vfs.com>
2023-02-17 17:50:19 -0800

    Created hacky solution for solving virtual joystick scaling  - used Constant pixel size - not maintainable solution

--- Assets/Scripts/UI/Mobile/VirtualJoystick.cs
+++ Assets/Scripts/UI/Mobile/VirtualJoystick.cs
@@ -9,7 +9,6 @@ public class VirtualJoystick : MonoBehaviour
 {
     [SerializeField] private Image m_VirtualJoystickImageBackground;
     [SerializeField] private Image m_VirtualJoystickImage;
-    [SerializeField] private TextMeshProUGUI m_DebugText;
 
     private EnhancedTouch.Finger m_CurrFinger = null;
     private float m_RadiusBackground;
@@ -23,11 +22,11 @@ public class VirtualJoystick : MonoBehaviour
     {
         // background values
         m_BackgroundRectTransform = m_VirtualJoystickImageBackground.transform.GetComponent<RectTransform>();
-        m_RadiusBackground = (m_BackgroundRectTransform.rect.width / 2) * m_BackgroundRectTransform.localScale.x;
+        m_RadiusBackground = (m_BackgroundRectTransform.rect.width / 2);// * m_BackgroundRectTransform.localScale.x;
 
         // joystick values
         m_JoystickRectTransform = m_VirtualJoystickImage.transform.GetComponent<RectTransform>();
-        m_RadiusJoystick = (m_JoystickRectTransform.rect.width / 2) * m_JoystickRectTransform.localScale.x * m_BackgroundRectTransform.localScale.x;
+        m_RadiusJoystick = (m_JoystickRectTransform.rect.width / 2);// * m_JoystickRectTransform.localScale.x * m_BackgroundRectTransform.localScale.x;
 
         m_AmountJoystickCanMove = m_RadiusBackground;// - m_RadiusJoystick;
 
@@ -51,11 +50,8 @@ public class VirtualJoystick : MonoBehaviour
             // Update joystick position and input values
             else
             {
-                Vector2 backgroundPos = new Vector2(m_BackgroundRectTransform.position.x, m_BackgroundRectTransform.position.y) +
-                    new Vector2(m_RadiusBackground, m_RadiusBackground);
+                Vector2 backgroundPos = new Vector2(m_BackgroundRectTransform.position.x, m_BackgroundRectTransform.position.y);
                 Vector2 touchVecFromCenter = m_CurrFinger.lastTouch.screenPosition - backgroundPos;
-
-                m_DebugText.SetText("Position: " + backgroundPos.ToString() + "\nVector: " + touchVecFromCenter.ToString());
 
                 // Scale Inputs to radius of background
                 Input.x = touchVecFromCenter.x / m_AmountJoystickCanMove;
@@ -93,8 +89,7 @@ public class VirtualJoystick : MonoBehaviour
         m_CurrFinger = finger;
         SetVisibility(true);
         // Set position of joystick to position touched, offset from size
-        m_BackgroundRectTransform.position = finger.lastTouch.screenPosition + 
-            new Vector2(-m_RadiusBackground, -m_RadiusBackground);
+        m_BackgroundRectTransform.position = finger.lastTouch.screenPosition;
     }
 
     private void SetVisibility(bool isVisible)
